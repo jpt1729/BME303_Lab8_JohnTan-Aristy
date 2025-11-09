61f3403f2cef78e452ab47e416a1d05599df6776
John <john.tan-aristy@csedge.org>
2025-11-08 22:30:11 -0600

    Fixes

--- BME303_Lab8_P1_JohnTan-Aristy.py
+++ BME303_Lab8_P1_JohnTan-Aristy.py
@@ -3,20 +3,21 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def coin_flip(dollars, tries):
-    for i in range(tries): # runs through every try
-        if random.randint(0, 1): # if its one then add one if not subtract one
-            dollars +=  1
+    for i in range(tries):  # runs through every try
+        if np.random.random() < 0.5:  # if its one then add one if not subtract one
+            dollars += 1
         else:
             dollars -= 1
     return dollars
 
 output = {}
-flips = [5, 50, 100, 500, 5000, 10000]
+flips = [5, 50, 100, 500, 1000, 5000, 10000]
+
 for flip in flips:
     temp = []
-    for gambler in range(1000): # number of gamblers
+    for gambler in range(1000):  # number of gamblers
         temp.append(coin_flip(100, flip))
-    output[flip] = np.array(temp)
+    output[flip] = np.array(temp) # saves the data as an np array
 
     print('--------------------')
     print(f'{flip} flips: ')
@@ -26,9 +27,10 @@ for flip in flips:
 
     plt.hist(output[flip], bins=32, color='skyblue', edgecolor='black')
 
+    # labelling the data
     plt.xlabel('Value')
     plt.ylabel('Frequency')
     plt.title(f'{flip} flips')
-
+    if flip == 10000:
+        plt.savefig('Lab8_P1_JohnTan-Aristy.png')
     plt.show()
-plt.savefig('Lab8_P1_JohnTan-Aristy.png')
